19d181a8db4a903b8b9f6cdc8b27cdc1a2ddbf94
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-03-13 18:27:18 +0100

    projetc structure

---
+++ Project1/Project1/Utilities/DataTypes/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Utilities.DataTypes
+{
+    public class Result
+    {
+        int memoryComplexity;
+        int computationalComplexity;
+        int index;
+
+        public Result()
+        {
+            memoryComplexity = 0;
+            computationalComplexity = 0;
+            index = 0;
+        }
+
+        public Result(int memoryComplexity, int computationalComplexity, int index)
+        {
+            this.memoryComplexity = memoryComplexity;
+            this.computationalComplexity = computationalComplexity;
+            this.index = index;
+        }
+    }
+}
---
+++ Project1/Project1/Utilities/Generators/TextGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Utilities.Generators
+{
+    internal class TextGenerator
+    {
+        public string Generate()
+        {
+            string text = string.Empty;
+
+            Random random = new Random();
+
+            text += ((char)random.Next(65, 90)).ToString();
+
+            return text;
+        }
+
+        public string Generate(int length)
+        {
+            string text = string.Empty;
+
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                text += ((char)random.Next(65, 90)).ToString();
+            }
+
+            return text;
+        }
+    }
+}
