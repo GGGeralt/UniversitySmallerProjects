48590e84523a01a2bc861d806e259679dfc00a96
GGGeralt <Bartek.lipinski.pl@gmail.com>
2023-04-27 13:42:46 +0200

    Project 3 base for 3

---
+++ Project4/Project4/Algorithmer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    internal class Algorithmer
+    {
+        List<string> points;
+
+        public int[,] distanceArray;
+
+        public Algorithmer(List<string> points, int[,] distances)
+        {
+            this.points = points;
+            distanceArray = distances;
+        }
+
+
+        /*
+         * for (int i = 0; i < distances.length; i++) {
+            for (int j = 0; j < distances.length; j++) {
+                for (int k = 0; k < distances.length; k++) {
+                    if ((distances[j][i] == MAX) || (distances[i][k] == MAX)) {
+                        continue;
+                    }
+                    distances[j][k] = Math.min(distances[j][j], distances[j][k] + distances[k][j]);
+                }
+            }
+        }
+        */
+        public void Run()
+        {
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (distanceArray[i, k] == int.MaxValue || distanceArray[k,j]==int.MaxValue)
+                        {
+                            continue;
+                        }
+                        distanceArray[i,j] = Math.Min(distanceArray[i, j], distanceArray[i,k] + distanceArray[k,j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        public void PrintState()
+        {
+            Console.WriteLine();
+            Console.Write("  ");
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Console.Write($"{points[i]}   ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                Console.Write($"{points[i]}|");
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (distanceArray[i,j] == int.MaxValue)
+                    {
+                        Console.Write($"MAX ");
+                        
+                    }
+                    else
+                    {
+                        Console.Write($"{distanceArray[i, j]}   ");
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Project4/Project4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Project4;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static int MAX = int.MaxValue;
+    static void Main()
+    {
+        List<string> points = new List<string>()
+        {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+        };
+        int[,] distances = new int[,]
+        {
+            {   0,   2,   4, MAX, MAX},
+            { MAX,   0,   3,   3, MAX},
+            { MAX, MAX,   0,  -1,   4},
+            { MAX, MAX, MAX,   0,   2},
+            { MAX, MAX, MAX, MAX,   0},
+        };
+
+        Algorithmer algorithmer = new Algorithmer(points, distances);
+
+        algorithmer.Run();
+
+        algorithmer.PrintState();
+    }
+}
